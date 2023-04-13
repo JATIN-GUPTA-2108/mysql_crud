6a09468818c3ef2a60718e739e6be21f4804693e
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2023-04-13 23:30:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated */
+
 const cors = require("cors");
 
 const app = express();
@@ -10,20 +10,20 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+app.use(express.json()); 
 
-// simple route
+
+app.use(express.urlencoded({ extended: true })); 
+
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
 });
 
 require("./app/routes/employee.routes.js")(app);
 
-// set port, listen for requests
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
